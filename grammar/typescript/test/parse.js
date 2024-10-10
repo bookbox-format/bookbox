c68191642ba271c19d118a901ee230e0f0fc8da4
nikalexxx <feedback@bookbox-format.ru>
2024-10-10 20:17:23 +0300

    add vscode

--- grammar/typescript/test/parse.js
+++ grammar/typescript/test/parse.js
@@ -1,11 +1,7 @@
-import { parse, stringify } from '../dist/index.js';
+import { getTokens, parse, stringify } from '../dist/index.js';
 import fs from 'node:fs';
 
-// const text = fs.readFileSync('./text.bbm', { encoding: 'utf-8' });
-const text = `
-just string
-
-{a.href{ya.ru} ya}
-`;
+const text = fs.readFileSync('./text.bbm', { encoding: 'utf-8' });
+// console.log(getTokens(text));
 fs.writeFileSync('./ast.json', JSON.stringify(parse(text), null, 2));
 console.log(stringify(parse(text)));
